7437edb3129fe61c2a6dd433151de444fba21da4
jenniferoentoro <104339219+jenniferoentoro@users.noreply.github.com>
2023-03-31 10:03:11 +0700

    cobs

--- js/app.js
+++ js/app.js
@@ -9,27 +9,9 @@ if ('serviceWorker' in navigator) {
 }
 
 window.addEventListener('beforeinstallprompt', function(event) {
-  console.log('beforeinstallprompt fired');
+  console.log('beforeinstallprompt firedde');
   event.preventDefault();
   deferredPrompt = event;
+  console.log(deferredPrompt);
   return false;
-});
-
-window.addEventListener('load', () => {
-  // If there is a deferredPrompt, show the install prompt
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
-
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User installed the PWA');
-      } else {
-        console.log('User dismissed the PWA install prompt');
-      }
-
-      // Clear the deferredPrompt so it can be used again later
-      deferredPrompt = null;
-    });
-  }
 });
--- js/start.js
+++ js/start.js
@@ -1,18 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    if (deferredPrompt) {
-      deferredPrompt.prompt();
-  
-      deferredPrompt.userChoice.then(function(choiceResult) {
-        console.log(choiceResult.outcome);
-  
-        if (choiceResult.outcome === 'dismissed') {
-          console.log('User cancelled installation');
-        } else {
-          console.log('User added to home screen');
-        }
-      });
-  
-      deferredPrompt = null;
-    }
-  });
-  
+console.log(deferredPrompt);
+
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+
+    deferredPrompt.userChoice.then(function(choiceResult) {
+      console.log(choiceResult.outcome);
+
+      if (choiceResult.outcome === 'dismissed') {
+        console.log('User cancelled installation');
+      } else {
+        console.log('User added to home screen');
+      }
+    });
+
+    deferredPrompt = null;
+  }
+
