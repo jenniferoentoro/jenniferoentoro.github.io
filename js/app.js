a886109aea6303936e862ef367b80bec82d79254
jenniferoentoro <104339219+jenniferoentoro@users.noreply.github.com>
2023-03-31 08:45:42 +0700

    xooxox

--- js/app.js
+++ js/app.js
@@ -16,18 +16,20 @@ window.addEventListener('beforeinstallprompt', function(event) {
   return false;
 });
 
-if (deferredPrompt) {
-  deferredPrompt.prompt();
+document.addEventListener('DOMContentLoaded', function() {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
 
-  deferredPrompt.userChoice.then(function(choiceResult) {
-    console.log(choiceResult.outcome);
+    deferredPrompt.userChoice.then(function(choiceResult) {
+      console.log(choiceResult.outcome);
 
-    if (choiceResult.outcome === 'dismissed') {
-      console.log('User cancelled installation');
-    } else {
-      console.log('User added to home screen');
-    }
-  });
+      if (choiceResult.outcome === 'dismissed') {
+        console.log('User cancelled installation');
+      } else {
+        console.log('User added to home screen');
+      }
+    });
 
-  deferredPrompt = null;
-}
+    deferredPrompt = null;
+  }
+});
--- js/start.js
+++ js/start.js
@@ -1,15 +1,18 @@
-if (deferredPrompt) {
-    deferredPrompt.prompt();
-
-    deferredPrompt.userChoice.then(function(choiceResult) {
-      console.log(choiceResult.outcome);
-
-      if (choiceResult.outcome === 'dismissed') {
-        console.log('User cancelled installation');
-      } else {
-        console.log('User added to home screen');
-      }
-    });
-
-    deferredPrompt = null;
-  }
+document.addEventListener('DOMContentLoaded', function() {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+  
+      deferredPrompt.userChoice.then(function(choiceResult) {
+        console.log(choiceResult.outcome);
+  
+        if (choiceResult.outcome === 'dismissed') {
+          console.log('User cancelled installation');
+        } else {
+          console.log('User added to home screen');
+        }
+      });
+  
+      deferredPrompt = null;
+    }
+  });
+  
